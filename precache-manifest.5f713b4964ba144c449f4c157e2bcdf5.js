f5d0d2973751349b1404475511c4ca05814f8afc
Maksim Hvatov <uzhas76064@gmail.com>
2020-08-15 23:13:30 +0500

    Updates

---
+++ precache-manifest.5f713b4964ba144c449f4c157e2bcdf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67592dd66e0a196f09fc20c2aa4f9c5",
+    "url": "/money-calc/index.html"
+  },
+  {
+    "revision": "e2825946217259cd251e",
+    "url": "/money-calc/static/css/main.e73bc210.chunk.css"
+  },
+  {
+    "revision": "b154c75e4a189bfc4a4b",
+    "url": "/money-calc/static/js/2.9aa87a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-calc/static/js/2.9aa87a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2825946217259cd251e",
+    "url": "/money-calc/static/js/main.bde43062.chunk.js"
+  },
+  {
+    "revision": "611656e889aaf6deccf9",
+    "url": "/money-calc/static/js/runtime-main.b91bbaa2.js"
+  }
+]);
